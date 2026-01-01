ac5bbcdb0486924b35a46518a93f3766791d4fa7
uint82 <abrorhilmi6@gmail.com>
2026-01-01 21:42:05 +0700

    feat: added blogs index

---
+++ src/ts/content/blogs-index.ts
@@ -0,0 +1,14 @@
+export const blogsIndex = [
+  {
+    slug: "my-first-post",
+    path: "/content/blogs/my-first-post.md",
+  },
+  {
+    slug: "learning-typescript",
+    path: "/content/blogs/learning-typescript.md",
+  },
+  {
+    slug: "spa-routing",
+    path: "/content/blogs/spa-routing.md",
+  },
+];

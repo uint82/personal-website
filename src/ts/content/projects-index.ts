5097776f35f51d3b51dc8c93a2d427922bafc426
uint82 <abrorhilmi6@gmail.com>
2026-01-01 21:42:35 +0700

    feat: added projects index

---
+++ src/ts/content/projects-index.ts
@@ -0,0 +1,14 @@
+export const projectsIndex = [
+  {
+    slug: "lazytype",
+    path: "/content/projects/lazytype.md",
+  },
+  {
+    slug: "task-manager",
+    path: "/content/projects/task-manager.md",
+  },
+  {
+    slug: "weather-app",
+    path: "/content/projects/weather-app.md",
+  },
+];

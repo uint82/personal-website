31b883c36ff10eb0d1a4a7305bd3453d8a3df70e
uint82 <abrorhilmi6@gmail.com>
2026-01-14 18:04:51 +0700

    feat: added index.ts

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,7 @@
 import "./controllers/route-controller";
 import "@fortawesome/fontawesome-free/css/all.css";
+import "highlight.js/styles/atom-one-dark.css";
+import "@simonwep/pickr/dist/themes/nano.min.css";
 import * as Header from "./pages/header";
 import * as Footer from "./pages/footer";
 import * as Nav from "./pages/nav";

f0525681745c3c284c573246a9a7920745c42125
uint82 <abrorhilmi6@gmail.com>
2026-01-14 17:18:04 +0700

    feat: added 404 page logic

--- src/ts/pages/404.ts
+++ src/ts/pages/404.ts
@@ -1,30 +1,17 @@
 import Page from "./page";
-import { navigate } from "../controllers/route-controller";
 
 class NotFoundPage extends Page {
-  private handleClick = (e: Event) => {
-    const target = e.target as HTMLElement;
-    if (target.id === "go-home-404") {
-      navigate("/");
-    }
-  };
-
-  async beforeShow(): Promise<void> {
-    document.removeEventListener("click", this.handleClick);
+  constructor() {
+    super({
+      name: "404",
+      pathname: "/404",
+      element: document.querySelector('[data-page="404"]') as HTMLElement,
+    });
   }
 
-  async afterShow(): Promise<void> {
-    document.addEventListener("click", this.handleClick);
-  }
-
-  async beforeHide(): Promise<void> {
-    document.removeEventListener("click", this.handleClick);
-  }
+  async beforeShow(): Promise<void> { }
+  async afterShow(): Promise<void> { }
+  async beforeHide(): Promise<void> { }
 }
 
-const element = document.querySelector('[data-page="404"]') as HTMLElement;
-export const page = new NotFoundPage({
-  name: "404",
-  pathname: "/404",
-  element: element,
-});
+export const page = new NotFoundPage();

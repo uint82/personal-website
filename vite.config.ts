f676c4206ce4dd4cdb654cac4e317a1c2966192f
uint82 <abrorhilmi6@gmail.com>
2026-01-15 08:10:09 +0700

    fix: build path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import injectHTML from "vite-plugin-html-inject";
 export default defineConfig(() => {
   return {
     root: "src",
+    build: {
+      outDir: "../dist",
+      emptyOutDir: true,
+    },
     envDir: "../",
     publicDir: "../static",
     plugins: [tailwindcss(), injectHTML()],
